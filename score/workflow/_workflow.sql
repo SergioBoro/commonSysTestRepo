97c896d833b5da714c0fbda548097838e1d85853
m.prudyvus <m.prudyvus@curs.ru>
2014-10-23 15:02:25 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@699 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/_workflow.sql
+++ score/workflow/_workflow.sql
@@ -13,32 +13,32 @@ CREATE TABLE form(
 );
 
 CREATE TABLE status(
-  id INT NOT NULL,
+  id VARCHAR(50) NOT NULL,
   name VARCHAR(100) NOT NULL,
-  modelId INT,
-  CONSTRAINT Pk_statuses PRIMARY KEY (id)
+  modelId VARCHAR(50) NOT NULL,
+  CONSTRAINT Pk_statuses PRIMARY KEY (id, modelId)
 );
 
 CREATE TABLE statusModel(
-  id INT NOT NULL,
+  id VARCHAR(50) NOT NULL,
   name VARCHAR(100) NOT NULL,
   CONSTRAINT Pk_statusModel PRIMARY KEY (id)
 );
 
 CREATE TABLE statusTransition(
-  id INT NOT NULL,
   name VARCHAR(100) NOT NULL,
-  statusFrom INT NOT NULL,
-  statusTo INT NOT NULL,
-  CONSTRAINT Pk_statusTransition PRIMARY KEY (id)
+  statusFrom VARCHAR(50) NOT NULL,
+  modelFrom VARCHAR(50) NOT NULL,
+  statusTo VARCHAR(50) NOT NULL,
+  modelTo VARCHAR(50) NOT NULL,
+  CONSTRAINT PK_statusTransition PRIMARY KEY (statusFrom, modelFrom, statusTo, modelTo)
 );
 
 -- *** FOREIGN KEYS ***
 ALTER TABLE status ADD CONSTRAINT Fk_status FOREIGN KEY (modelId) REFERENCES workflow.statusModel(id);
-ALTER TABLE statusTransition ADD CONSTRAINT Fk_statusTransition FOREIGN KEY (statusFrom) REFERENCES workflow.status(id);
-ALTER TABLE statusTransition ADD CONSTRAINT Fk_statusTransition_0 FOREIGN KEY (statusTo) REFERENCES workflow.status(id);
+ALTER TABLE statusTransition ADD CONSTRAINT fk_statustransition_status FOREIGN KEY (statusFrom, modelFrom) REFERENCES workflow.status(id, modelId);
+ALTER TABLE statusTransition ADD CONSTRAINT fk_statustransition_status2 FOREIGN KEY (statusTo, modelTo) REFERENCES workflow.status(id, modelId);
 -- *** INDICES ***
-CREATE INDEX idx_status ON status(modelId);
-CREATE INDEX idx_statusTransition ON statusTransition(statusFrom);
-CREATE INDEX idx_statusTransition_0 ON statusTransition(statusTo);
+CREATE INDEX idx_statusTransition ON statusTransition(statusFrom, modelFrom);
+CREATE INDEX idx_statusTransition_0 ON statusTransition(statusTo, modelTo);
 -- *** VIEWS ***
