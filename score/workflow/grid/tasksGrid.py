f1a0b725582bef68a5c2032212f8f77e065de175
m.prudyvus <m.prudyvus@curs.ru>
2014-10-23 15:03:02 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@702 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/grid/tasksGrid.py
+++ score/workflow/grid/tasksGrid.py
@@ -16,10 +16,6 @@ try:
 except:
     from ru.curs.celesta.showcase import JythonDTO, JythonDownloadResult
 
-try:
-    from ru.curs.showcase.activiti import  EngineFactory
-except:
-    from workflow import testConfig as EngineFactory
 
 
 def gridDataAndMeta(context, main=None, add=None, filterinfo=None,
@@ -32,51 +28,71 @@ def gridDataAndMeta(context, main=None, add=None, filterinfo=None,
 
     data = {"records":{"rec":[]}}
     _header = {"id":["~~id"],
-               "ID":["ID"],
+               "schema":[u"Схема"],
                "name":[u"Название задачи"],
+               "process": [u"Название процесса"],
+               "docName": [u"Название процесса"],
                "properties":[u"properties"]}
 
     for column in _header:
         _header[column].append(toHexForXml(_header[column][0]))
     # Проходим по таблице и заполняем data
-    processEngine = EngineFactory.getActivitiProcessEngine()
-    taskService = processEngine.getTaskService();
+    taskService = activiti.taskService
     for tasks in tasksList:
         procDict = {}
         procDict[_header["id"][1]] = tasks.id
         task = taskService.createTaskQuery().taskId(tasks.id).singleResult()
-#         process = task.getProcessInstanceId()
-#         procDict[_header["process"][1]] = tasks.id
+        processInstanceId = task.getProcessInstanceId()
+        processInstance = activiti.runtimeService.createProcessInstanceQuery()\
+            .processInstanceId(processInstanceId).singleResult()
+        processDefinition = activiti.repositoryService.createProcessDefinitionQuery()\
+            .processDefinitionId(processInstance.getProcessDefinitionId()).singleResult()
+        docName = "%s. %s" % (taskService.getVariable(tasks.id, 'docId'), taskService.getVariable(tasks.id, 'docName'))
+        procDict[_header["process"][1]] = "%s: %s" % (processDefinition.getName(), docName)
+        procDict[_header["schema"][1]] = {"div":
+                                           {"@align": "center",
+                                            "img":
+                                            {"@src": "solutions/default/resources/flowblock.png", "@height": "20px"}}}
         procDict[_header["name"][1]] = tasks.name
-        procDict[_header["properties"][1]] = {"event":{"@name":"row_single_click",
-                                         "action":{"@show_in": "MODAL_WINDOW",
-                                                  "#sorted":[{"main_context":"current"},
-                                                             {"modalwindow":{"@caption": "Схема процесса"
-                                                                             }
-                                                              },
-                                                             {"datapanel":{"@type": "current",
-                                                                           "@tab": "current",
-                                                                           "element": {"@id": "tasksImage"}
-                                                                           }
-                                                              }
-                                                             ]
-                                                  }
-                                         }
-                                }
+
+        procDict[_header["properties"][1]] = {"event":
+                                              {"@name":"cell_single_click",
+                                               "@column": _header["schema"][0],
+                                               "action":
+                                                    {"@show_in": "MODAL_WINDOW",
+                                                     "#sorted":
+                                                        [{"main_context":"current"},
+                                                         {"modalwindow":
+                                                            {"@caption": "Схема процесса"
+                                                             }
+                                                          },
+                                                         {"datapanel":
+                                                            {"@type": "current",
+                                                             "@tab": "current",
+                                                             "element":
+                                                                {"@id": "tasksImage"
+                                                                 }
+                                                             }
+                                                          }
+                                                         ]
+                                                     }
+                                               }
+                                              }
         data["records"]["rec"].append(procDict)
 
     # Определяем список полей таблицы для отображения
     settings = {}
     settings["gridsettings"] = {"columns": {"col":[]},
                                 "properties": {"@pagesize":"50",
-                                               "@gridWidth": "1200px",
-                                               "@gridHeight": "300",
+                                               "@gridWidth": "1000px",
+                                               "@gridHeight": "500",
                                                "@totalCount": len(tasksList),
-                                               "@profile":"default.properties"},
-                                "labels":{"header":"Список задач пользователя"}
+                                               "@profile":"default.properties"}
                                 }
     # Добавляем поля для отображения в gridsettings
-    settings["gridsettings"]["columns"]["col"].append({"@id":_header["name"][0], "@width": "300px"})
+    settings["gridsettings"]["columns"]["col"].append({"@id":_header["schema"][0], "@width": "40px", "type": "IMAGE"})
+    settings["gridsettings"]["columns"]["col"].append({"@id":_header["name"][0], "@width": "280px"})
+    settings["gridsettings"]["columns"]["col"].append({"@id":_header["process"][0], "@width": "650px"})
 
     res1 = XMLJSONConverter.jsonToXml(json.dumps(data))
     res2 = XMLJSONConverter.jsonToXml(json.dumps(settings))
