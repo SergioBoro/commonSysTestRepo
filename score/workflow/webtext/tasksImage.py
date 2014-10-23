20facb1259aa2730f2cd760d63db99c55d1da043
m.prudyvus <m.prudyvus@curs.ru>
2014-10-23 15:02:53 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@701 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/webtext/tasksImage.py
+++ score/workflow/webtext/tasksImage.py
@@ -8,28 +8,20 @@ Created on 22.10.2014
 
 
 import simplejson as json
-import base64
+
 from common.sysfunctions import toHexForXml
 from ru.curs.celesta.showcase.utils import XMLJSONConverter
 from workflow.processUtils import ActivitiObject, getBase64Image
-import javax.xml.stream.XMLInputFactory as XMLInputFactory
-import java.io.InputStreamReader as InputStreamReader
-import org.activiti.bpmn.converter.BpmnXMLConverter as BpmnXMLConverter
+
 try:
     from ru.curs.showcase.core.jython import JythonDTO
 except:
     from ru.curs.celesta.showcase import JythonDTO
 
-try:
-    from ru.curs.showcase.activiti import  EngineFactory
-except:
-    from workflow import testConfig as EngineFactory
-
 def webtextData(context, main=None, add=None, filterinfo=None,
              session=None, elementId=None):
-    processEngine = EngineFactory.getActivitiProcessEngine()
-    taskService = processEngine.getTaskService();
     activiti = ActivitiObject()
+    taskService = activiti.taskService;
 
     session = json.loads(session)
     taskId = session['sessioncontext']['related']['gridContext']['currentRecordId']
