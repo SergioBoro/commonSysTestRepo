afdd726be8af9a7e6d758b82eb3d332c3a037d17
m.prudyvus <m.prudyvus@curs.ru>
2014-10-23 12:54:09 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@695 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

---
+++ score/workflow/datapanel/tasks.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+'''
+Created on 21.10.2014
+
+@author: m.prudyvus
+'''
+
+from ru.curs.celesta.showcase.utils import XMLJSONConverter
+import simplejson as json
+
+def myTasks(context, main=None, session=None):
+    data = {"datapanel":{"tab":{"@id":"1",
+                                "@name":u"Список задач",
+                                "element":[{"@id":"tasksGrid",
+                                            "@type":"grid",
+                                            "@proc":"workflow.grid.tasksGrid.gridDataAndMeta.celesta",
+                                            "@subtype":"JS_LIVE_GRID",
+                                            "@plugin":"liveDGrid",
+                                            "proc":[{
+                                                    "@id":1,
+                                                    "@name":"workflow.grid.tasksGrid.gridToolBar.celesta",
+                                                    "@type":"TOOLBAR"}]
+                                            },
+                                           {"@id":"tasksImage",
+                                            "@type":"webtext",
+                                            "@hideOnLoad":"true",
+                                            "@neverShowInPanel":"true",
+                                            "@proc":"workflow.webtext.tasksImage.webtextData.celesta",
+                                            "related":{"@id":"tasksGrid"}
+                                            },
+                                           {"@id":"taskStatusCard",
+                                            "@type":"xforms",
+                                            "@neverShowInPanel":"true",
+                                            "@template": "tasksStatus.xml",
+                                            "@proc":"workflow.xforms.tasksStatus.cardData.celesta",
+                                            "related":{"@id":"tasksGrid"},
+                                            "proc": {"@id": "1",
+                                                     "@name": "workflow.xforms.tasksStatus.cardDataSave.celesta",
+                                                     "@type": "SAVE"}
+                                            }
+                                           ]
+                                }
+                         }
+            }
+    return XMLJSONConverter.jsonToXml(json.dumps(data))
