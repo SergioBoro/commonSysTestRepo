cad828790362231e31cea5c71763c6edce655be4
m.prudyvus <m.prudyvus@curs.ru>
2014-11-05 16:13:32 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@754 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

---
+++ score/workflow/datapanel/tasks.py
@@ -0,0 +1,167 @@
+# coding: utf-8
+
+'''
+Created on 21.10.2014
+
+@author: m.prudyvus
+'''
+
+from ru.curs.celesta.showcase.utils import XMLJSONConverter
+import simplejson as json
+
+def activeTasks(context, main=None, session=None):
+    data = {"datapanel":{"tab":{"@id":"1",
+                                "@name":u"Список задач",
+                                "element":[{"@id":"tasksFilter",
+                                            "@type":"xforms",
+                                            "@template": "tasksFilter.xml",
+                                            "@proc":"workflow.xforms.tasksFilter.activeTasksFilter.celesta",
+                                            },
+                                           {"@id":"tasksGrid",
+                                            "@type":"grid",
+                                            "@proc":"workflow.grid.activeTasksGrid.gridDataAndMeta.celesta",
+                                            "@subtype":"JS_LIVE_GRID",
+                                            "@plugin":"liveDGrid",
+                                            "related":{"@id":"tasksFilter"},
+                                            "proc":[{
+                                                    "@id":1,
+                                                    "@name":"workflow.grid.activeTasksGrid.gridToolBar.celesta",
+                                                    "@type":"TOOLBAR"}]
+                                            },
+                                           {"@id":"tasksImage",
+                                            "@type":"webtext",
+                                            "@hideOnLoad":"true",
+                                            "@neverShowInPanel":"true",
+                                            "@proc":"workflow.webtext.tasksImage.webtextData.celesta",
+                                            "related":{"@id":"tasksGrid"}
+                                            },
+                                           {"@id":"taskStatusCard",
+                                            "@type":"xforms",
+                                            "@neverShowInPanel":"true",
+                                            "@template": "tasksStatus.xml",
+                                            "@proc":"workflow.xforms.tasksStatus.cardData.celesta",
+                                            "related":{"@id":"tasksGrid"},
+                                            "proc": {"@id": "1",
+                                                     "@name": "workflow.xforms.tasksStatus.cardDataSave.celesta",
+                                                     "@type": "SAVE"}
+                                            },
+                                           {"@id":"reassign",
+                                            "@type":"xforms",
+                                            "@neverShowInPanel":"true",
+                                            "@template": "tasksReassign.xml",
+                                            "@proc":"workflow.xforms.tasksReassign.cardData.celesta",
+                                            "related":{"@id":"tasksGrid"},
+                                            "proc": {"@id": "1",
+                                                     "@name": "workflow.xforms.tasksReassign.cardDataSave.celesta",
+                                                     "@type": "SAVE"}
+                                            }
+                                           ]
+                                }
+                         }
+            }
+    return XMLJSONConverter.jsonToXml(json.dumps(data))
+
+def allActiveTasks(context, main=None, session=None):
+    data = {"datapanel":{"tab":{"@id":"1",
+                                "@name":u"Список задач",
+                                "element":[{"@id":"tasksFilter",
+                                            "@type":"xforms",
+                                            "@template": "tasksFilter.xml",
+                                            "@proc":"workflow.xforms.tasksFilter.allActiveTasksFilter.celesta",
+                                            },
+                                           {"@id":"tasksGrid",
+                                            "@type":"grid",
+                                            "@proc":"workflow.grid.allActiveTasksGrid.gridDataAndMeta.celesta",
+                                            "@subtype":"JS_LIVE_GRID",
+                                            "@plugin":"liveDGrid",
+                                            "related":{"@id":"tasksFilter"},
+                                            "proc":[{
+                                                    "@id":1,
+                                                    "@name":"workflow.grid.allActiveTasksGrid.gridToolBar.celesta",
+                                                    "@type":"TOOLBAR"}]
+                                            },
+                                           {"@id":"tasksImage",
+                                            "@type":"webtext",
+                                            "@hideOnLoad":"true",
+                                            "@neverShowInPanel":"true",
+                                            "@proc":"workflow.webtext.tasksImage.webtextData.celesta",
+                                            "related":{"@id":"tasksGrid"}
+                                            },
+                                           {"@id":"taskStatusCard",
+                                            "@type":"xforms",
+                                            "@neverShowInPanel":"true",
+                                            "@template": "tasksStatus.xml",
+                                            "@proc":"workflow.xforms.tasksStatus.cardData.celesta",
+                                            "related":{"@id":"tasksGrid"},
+                                            "proc": {"@id": "1",
+                                                     "@name": "workflow.xforms.tasksStatus.cardDataSave.celesta",
+                                                     "@type": "SAVE"}
+                                            }
+                                           ]
+                                }
+                         }
+            }
+    return XMLJSONConverter.jsonToXml(json.dumps(data))
+
+def archiveTasks(context, main=None, session=None):
+    data = {"datapanel":{"tab":{"@id":"1",
+                                "@name":u"Список задач",
+                                "element":[{"@id":"tasksFilter",
+                                            "@type":"xforms",
+                                            "@template": "tasksFilter.xml",
+                                            "@proc":"workflow.xforms.tasksFilter.archiveTasksFilter.celesta",
+                                            },
+                                           {"@id":"tasksGrid",
+                                            "@type":"grid",
+                                            "@proc":"workflow.grid.archiveTasksGrid.gridDataAndMeta.celesta",
+                                            "@subtype":"JS_LIVE_GRID",
+                                            "@plugin":"liveDGrid",
+                                            "related":{"@id":"tasksFilter"}
+                                            },
+                                           {"@id":"tasksImage",
+                                            "@type":"webtext",
+                                            "@hideOnLoad":"true",
+                                            "@neverShowInPanel":"true",
+                                            "@proc":"workflow.webtext.tasksImage.webtextData.celesta",
+                                            "related":{"@id":"tasksGrid"}
+                                            }
+                                           ]
+                                }
+                         }
+            }
+    return XMLJSONConverter.jsonToXml(json.dumps(data))
+
+
+def standardCompleteTask(context, main=None, session=None):
+    u'''Датапанель стандартного завершения задачи'''
+    data = {"datapanel":
+            {"tab":
+             {"@id":"completeTask",
+              "@name":u"Завершение задачи",
+              "element":
+                [{"@id":"completeTaskCard",
+                  "@type":"xforms",
+                  "@proc":"workflow.xforms.standardCompleteTaskCard.cardData.celesta",
+                  "@template":"workflow/standardCompleteTaskCard.xml",
+                  "proc":
+                    [{"@id":"completeTaskCardSave",
+                      "@name":"workflow.xforms.standardCompleteTaskCard.cardSave.celesta",
+                      "@type":"SAVE"}]}]}}}
+    return XMLJSONConverter.jsonToXml(json.dumps(data))
+
+def standardCompleteTaskWithStatus(context, main=None, session=None):
+    u'''Датапанель стандартного завершения задачи'''
+    data = {"datapanel":
+            {"tab":
+             {"@id":"completeTask",
+              "@name":u"Завершение задачи",
+              "element":
+                [{"@id":"completeTaskCard",
+                  "@type":"xforms",
+                  "@proc":"workflow.xforms.standardCompleteTaskStCard.cardData.celesta",
+                  "@template":"workflow/standardCompleteTaskStCard.xml",
+                  "proc":
+                    [{"@id":"completeTaskCardSave",
+                      "@name":"workflow.xforms.standardCompleteTaskStCard.cardDataSave.celesta",
+                      "@type":"SAVE"}]}]}}}
+    return XMLJSONConverter.jsonToXml(json.dumps(data))
