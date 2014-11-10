aa2c84e091c7afd8c1fd3af73f9395e43995f449
s.reshetnyak <s.reshetnyak@curs.ru>
2014-11-10 13:40:49 +0000

    Триггер на таблицу согласований процесса
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@790 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/__init__.py
+++ score/workflow/__init__.py
@@ -3,6 +3,14 @@
 from common.navigator import navigatorsParts
 from workflow.navigator import manageProcessesNav, navSettings, testNavigator
 
+from workflow.xforms.addMatcher import matchingCircuitPreInsert
+
+from . import _workflow_orm
+
+
 navigatorsParts['54'] = manageProcessesNav
 #navigatorsParts['55'] = testNavigator
 navigatorsParts['__header__'] = navSettings
+
+_workflow_orm.matchingCircuitCursor.onPreInsert.append(matchingCircuitPreInsert)
+_workflow_orm.matchingCircuitCursor.onPreUpdate.append(matchingCircuitPreInsert)
