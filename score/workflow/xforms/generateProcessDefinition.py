62c0cc2f5bf29307f61392f9bc7a74f5c626ad47
s.reshetnyak <s.reshetnyak@curs.ru>
2014-11-14 13:20:10 +0000

    Карточка разворачивания процесса, сформированного по схеме согласования
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@825 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/xforms/generateProcessDefinition.py
+++ score/workflow/xforms/generateProcessDefinition.py
@@ -23,6 +23,17 @@ except:
     from ru.curs.celesta.showcase import JythonDTO
 
 try:
+    from ru.curs.showcase.activiti import  EngineFactory
+except:
+    from workflow import testConfig as EngineFactory
+
+from java.io import ByteArrayInputStream
+
+from workflow._workflow_orm import matchingCircuitCursor
+
+from workflow.webtext.generateProcessImage import getProcessXML
+
+try:
     from ru.curs.showcase.core.selector import ResultSelectorData
     from ru.beta2.extra.gwt.ui.selector.api import DataRecord
 except:
@@ -32,13 +43,34 @@ from ru.curs.celesta.showcase.utils import XMLJSONConverter
 
 def cardData(context, main=None, add=None, filterinfo=None, session=None, elementId=None):
     u'''Карточка выбора процесса'''
+    session = json.loads(session)
+    processKey = session['sessioncontext']['related']['xformsContext']['formData']['schema']['data']['@processKey']
+    processName = session['sessioncontext']['related']['xformsContext']['formData']['schema']['data']['@processName']
+    matchingCircuit = matchingCircuitCursor(context)
+    matchingCircuitClone = matchingCircuitCursor(context)
+    matchingCircuit.setRange('processKey',processKey)
+    allowed = 'false'
+    if matchingCircuit.count() == 0:
+        pass
+    else:
+        matchingCircuitClone.setRange('processKey',processKey)
+        matchingCircuit.setRange('type','parallel')
+        parallelFlag = True
+        #Проверка на то, что в каждом параллельном согласовании не менее двух задач
+        for matchingCircuit in matchingCircuit.iterate():
+            matchingCircuitClone.setFilter('number',"'%s.'%%" % matchingCircuit.number)
+            if matchingCircuitClone.count() < 2:
+                parallelFlag = False
+                break
+        if parallelFlag:
+            allowed = 'true'
+    
+    
     xformsdata = {"schema":
                     {"@xmlns":"",
                      "data":
                         {
-                         "@processName": "",
-                         "@processKey": "",
-                         "@existing": "false"
+                            "@allowed":allowed
                          }
                      }
                   }
@@ -69,6 +101,25 @@ def cardData(context, main=None, add=None, filterinfo=None, session=None, elemen
     jsonSettings = XMLJSONConverter.jsonToXml(json.dumps(xformssettings))
     return JythonDTO(jsonData, jsonSettings)
 
-
+def cardSave(context, main, add, filterinfo, session, elementId, data):
+    u'''Запуск процесса'''
+    session = json.loads(session)
+    processKey = session['sessioncontext']['related']['xformsContext']['formData']['schema']['data']['@processKey']
+    processName = session['sessioncontext']['related']['xformsContext']['formData']['schema']['data']['@processName']
+    matchingCircuit = matchingCircuitCursor(context)
+    matchingCircuitClone = matchingCircuitCursor(context)
+    processKey = session['sessioncontext']['related']['xformsContext']['formData']['schema']['data']['@processKey']
+    processName = session['sessioncontext']['related']['xformsContext']['formData']['schema']['data']['@processName']
+    matchingCircuit.setRange('processKey',processKey)
+    matchingCircuit.setRange('processKey',processKey)
+    matchingCircuit.setFilter('number',"!%'.'%")
+    matchingCircuit.orderBy('sort')
+    matchingCircuitClone.setRange('processKey',processKey)
+    stream = ByteArrayInputStream(getProcessXML(context,matchingCircuit,matchingCircuitClone, processKey, processName).encode('utf-8'))
+    processEngine = EngineFactory.getActivitiProcessEngine()
+    repositoryService = processEngine.getRepositoryService()
+    repositoryService.createDeployment().addInputStream(processName+'.bpmn', stream).deploy()
+    return context.message(u'Процесс развёрнут')
+    
 
 
