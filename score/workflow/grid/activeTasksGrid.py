bb8963c5709f22a49827bf8ce91244354f093e6a
m.prudyvus <m.prudyvus@curs.ru>
2014-11-17 16:47:46 +0000

    formType
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@847 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/grid/activeTasksGrid.py
+++ score/workflow/grid/activeTasksGrid.py
@@ -107,7 +107,7 @@ def gridDataAndMeta(context, main=None, add=None, filterinfo=None,
         taskDict[_header["schema"][1]] = {"div":
                                             {"@align": "center",
                                              "a":
-                                             {"@href": "./?userdata=%s&mode=image&processId=%s" \
+                                             {"@href": "./?userdata=%s&mode=image&processId=%&formType=approve" \
                                                             % (session["userdata"], processInstanceId),
                                               "@target": "_blank",
                                               "img":
