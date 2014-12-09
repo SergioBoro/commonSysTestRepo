a60f5a3f214148db0fedfb6232e4751ebe3a1572
s.reshetnyak <s.reshetnyak@curs.ru>
2014-12-09 14:26:31 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@930 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/tables/tablesInit.py
+++ score/workflow/tables/tablesInit.py
@@ -20,12 +20,11 @@ from ru.curs.celesta import SessionContext
 
 
 def importData(tableInstance, filePath):
-    u'''Функция загрузки данных в БД'''
-    if tableInstance.count() == 0:
-        dataStream = FileInputStream(filePath)    
-        exchange = DataBaseXMLExchange(dataStream, tableInstance)
-        exchange.uploadXML()
-        dataStream.close()
+    u'''Функция загрузки данных в БД''' 
+    dataStream = FileInputStream(filePath)    
+    exchange = DataBaseXMLExchange(dataStream, tableInstance)
+    exchange.uploadXML()
+    dataStream.close()
         
 def exportData(tableInstance,path):
     dataStream= FileOutputStream(path)     
@@ -36,44 +35,55 @@ def exportData(tableInstance,path):
 def initTables():
     a = Celesta.getInstance()
     conn = ConnectionPool.get()  
-    sesContext = SessionContext('master', 'mastersession')
+    sesContext = SessionContext('super', 'supersession')
     context = CallContext(conn, sesContext)
     
     filePath = os.path.dirname(os.path.abspath(__file__))
     
+    initializationRequired = False
+    
     roles = RolesCursor(context)
-    importData(roles,filePath+'/roles.xml')
     
-    userroles = UserRolesCursor(context)
-    importData(userroles,filePath+'/userRoles.xml')    
     
-    perms = PermissionsCursor(context)
-    importData(perms,filePath+'/permissions.xml')
-
-
+    if roles.tryGet('reader'):
+        if roles.tryGet('editor'):
+            if roles.count() == 2:
+                initializationRequired = True
+    if initializationRequired:
+        importData(roles,filePath+'/roles.xml')
+        
+        userroles = UserRolesCursor(context)
+        importData(userroles,filePath+'/userRoles.xml')    
+        
+        perms = PermissionsCursor(context)
+        importData(perms,filePath+'/permissions.xml')
     
-    importTables = [
-                {'grain':'security','table':'customPerms'},
-                {'grain':'security','table':'logins'},
-                {'grain':'security','table':'subjects'},
-                {'grain':'security','table':'rolesCustomPerms'},
-                {'grain':'workflow','table':'form'},
-                {'grain':'workflow','table':'processes'},
-                {'grain':'workflow','table':'matchingCircuit'},
-                {'grain':'common','table':'linesOfNumbersSeries'},
-                {'grain':'common','table':'numbersSeries'},
-                #{'grain':'workflow','table':'userGroup'}
-                ]            
-
-    for table in importTables:
-        tableInstance = tableCursorImport(table['grain'], table['table'])(context)
-        importData(tableInstance,filePath+'/'+table['table']+'.xml')
-    context.commit()
+    
+        
+        importTables = [
+                    {'grain':'security','table':'customPermsTypes'},
+                    {'grain':'security','table':'customPerms'},
+                    {'grain':'security','table':'logins'},
+                    {'grain':'security','table':'subjects'},
+                    {'grain':'security','table':'rolesCustomPerms'},
+                    {'grain':'workflow','table':'form'},
+                    {'grain':'workflow','table':'processes'},
+                    {'grain':'workflow','table':'matchingCircuit'},
+                    {'grain':'common','table':'numbersSeries'},
+                    {'grain':'common','table':'linesOfNumbersSeries'},
+                    
+                    #{'grain':'workflow','table':'userGroup'}
+                    ]            
+    
+        for table in importTables:
+            tableInstance = tableCursorImport(table['grain'], table['table'])(context)
+            importData(tableInstance,filePath+'/'+table['table']+'.xml')
+        context.commit()
         
 def exportTables():
     a = Celesta.getInstance()
     conn = ConnectionPool.get()  
-    sesContext = SessionContext('master', 'mastersession')
+    sesContext = SessionContext('super', 'supersession')
     context = CallContext(conn, sesContext)
     
     filePath = os.path.dirname(os.path.abspath(__file__))
@@ -88,6 +98,7 @@ def exportTables():
     exportData(userroles,filePath+'/userRoles.xml')
     
     exportTables = [
+                    {'grain':'security','table':'customPermsTypes'},
                     {'grain':'security','table':'customPerms'},
                     {'grain':'security','table':'logins'},
                     {'grain':'security','table':'subjects'},
