4a62123bdac2df80b2e0b3eb37d16e3785b1e724
s.reshetnyak <s.reshetnyak@curs.ru>
2014-12-09 12:06:52 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@927 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/__init__.py
+++ score/workflow/__init__.py
@@ -5,12 +5,16 @@ from workflow.navigator import manageProcessesNav, navSettings, testNavigator
 
 from workflow.xforms.addMatcher import matchingCircuitPreInsert
 
+from workflow.tables.tablesInit import initTables
+
 from . import _workflow_orm
 
 
 navigatorsParts['54'] = manageProcessesNav
 #navigatorsParts['55'] = testNavigator
 navigatorsParts['__header__'] = navSettings
+
+initTables()
 
 _workflow_orm.matchingCircuitCursor.onPreInsert.append(matchingCircuitPreInsert)
 _workflow_orm.matchingCircuitCursor.onPreUpdate.append(matchingCircuitPreInsert)
