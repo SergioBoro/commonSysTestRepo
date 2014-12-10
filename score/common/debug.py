1ec20f2aafc5a9845753368b10227de002171dd3
AleXXL <alexxl@curs.ru>
2014-12-10 12:02:13 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@932 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/debug.py
+++ score/common/debug.py
@@ -4,29 +4,36 @@ from ru.curs.celesta import Celesta
 from ru.curs.celesta.score import Score
 from ru.curs.celesta import ConnectionPool
 from ru.curs.celesta import CallContext
-from common import navigator, sysfunctions
-from common.xmlutils import XMLJSONConverter
-
+from common import navigator, sysfunctions, hierarchy
+from common import settings
+from ru.curs.celesta import SessionContext
+from datetime import datetime
 #try:
 #    from java.lang import String111
 #except:
 #    from java.lang import Integer as III
-
-
+from mytest._mytest_orm import testTableCursor
 import sys, os
 import common
 #from security.grid import permissions
 
 a = Celesta.getInstance()
 conn = ConnectionPool.get()
-context = CallContext(conn, 'admin', a)
+sesContext = SessionContext('admin', 'testsession')
+context = CallContext(conn, sesContext)
 #print context.getCelesta()
-from security.xform import permissions
+
+
+
 
 def proc1(context):
-    print permissions.cardData(context, add='add').data
+    print datetime.now()
+    aa = settings.settingsManager()
 
+    print aa.getGrainSettings('common', '''navigator/userdata/@name''')
+    print datetime.now()
 proc1(context)
+
 #    from dirusing._dirusing_orm import filtersConditionsCursor
 #    filtersConditions = filtersConditionsCursor(context)
 #    filtersConditions.id = 1
