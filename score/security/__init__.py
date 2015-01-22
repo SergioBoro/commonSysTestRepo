8e1ddd5e4653eaba60bc9e23355d331ae784dba6
AleXXL <alexxl@curs.ru>
2015-01-22 16:44:06 +0000

    исправление зависания при инициализации
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@988 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/security/__init__.py
+++ score/security/__init__.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 from common.navigator import navigatorsParts
 from ru.curs.celesta.syscursors import RolesCursor
 from security.functions import Settings
@@ -8,22 +7,33 @@ from security.setForeignKeys import setForeignKeys as setConstraint
 from security.xform.users import employeesSubjectsPostInsert, employeesSubjectsPostUpdate, employeesSubjectsPreDelete
 from common.sysfunctions import tableCursorImport
 from security.securityInit import securityInit
+from ru.curs.celesta import ConnectionPool
+from ru.curs.celesta import CallContext
+from ru.curs.celesta import SessionContext
+from ru.curs.celesta import Celesta
 
-
-settings=Settings()
+settings = Settings()
 if not settings.loginIsSubject():
-    employeesGrain=settings.getEmployeesParam("employeesGrain")
-    employeesTable=settings.getEmployeesParam("employeesTable")
+    employeesGrain = settings.getEmployeesParam("employeesGrain")
+    employeesTable = settings.getEmployeesParam("employeesTable")
     if settings.isEmployees():
-        employeesCursor=tableCursorImport(employeesGrain, employeesTable)
-        
+        employeesCursor = tableCursorImport(employeesGrain, employeesTable)
+
         employeesCursor.onPostInsert.append(employeesSubjectsPostInsert)
         employeesCursor.onPostUpdate.append(employeesSubjectsPostUpdate)
         employeesCursor.onPreDelete.append(employeesSubjectsPreDelete)
-    
-if not settings.isSystemInitialised():    
-    if settings.isEmployees():
-        setConstraint() #функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
-    securityInit()
 
+if not settings.isSystemInitialised():
+    a = Celesta.getInstance()
+    adminUser = settings.getEmployeesParam("admin")
+    conn = ConnectionPool.get()
+    try:
+        sesContext = SessionContext(adminUser, 'initsession')
+        context = CallContext(conn, sesContext)
+        if settings.isEmployees():
+            setConstraint(context) #функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
+        securityInit(context)
+    finally:
+        ConnectionPool.putBack(conn)
 navigatorsParts['99'] = authentificationNavigator
+
