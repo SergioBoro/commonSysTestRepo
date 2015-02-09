a34067cdc074a7cd883621085677343dcfae44e2
s.reshetnyak <s.reshetnyak@curs.ru>
2015-02-09 09:40:36 +0000

    К одному событию activiti можно привязать несколько обработчиков событий. Также, в случае необходимости, инициализируется таблица act_ge_property.
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@1010 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/workflow/__init__.py
+++ score/workflow/__init__.py
@@ -26,6 +26,31 @@ context = initcontext()
 #Заполнение таблиц начальными данными, производится только в случае первого разворачивания решения
 initTables(context)
 
+#Заполнение версии активити. Необходим для коррекной работы activiti
+#coding:utf-8
+
+
+from workflow._workflow_orm import act_ge_propertyCursor
+
+act_property = act_ge_propertyCursor(context)
+
+if not act_property.tryGet('schema.version'):
+    if not act_property.tryGet('schema.history'):
+        if not act_property.tryGet('next.dbid'):
+            act_property.name_ = 'schema.version'
+            act_property.value_ = '5.16' 
+            act_property.rev_ = 1
+            act_property.insert()
+            act_property.name_ = 'schema.history'
+            act_property.value_ = 'create(5.16)' 
+            act_property.rev_ = 1
+            act_property.insert()
+            act_property.name_ = 'next.dbid'
+            act_property.value_ = '1'
+            act_property.rev_ = 1
+            act_property.insert()
+
+
 
 #Настройка обработчиков событий activiti
 
@@ -33,7 +58,7 @@ initTables(context)
 try:
     
     from org.activiti.engine.delegate.event import ActivitiEventType    
-    
+    from java.util import ArrayList
     from ru.curs.showcase.runtime import AppInfoSingleton
 
 
@@ -43,12 +68,22 @@ try:
     eventsList = settingsManager.getGrainSettings('activitiEvents/event/@name','workflow')
     handlersList = settingsManager.getGrainSettings('activitiEvents/event/@script','workflow')
     
-    
+    eventsDict = dict()
     for i in range(len(eventsList)):
+        if eventsList[i] not in eventsDict:
+            eventsDict[eventsList[i]] = ArrayList()
+        eventsDict[eventsList[i]].add(handlersList[i])
+        
+
+    if 'TASK_CREATED' not in eventsDict:
+        eventsDict['TASK_CREATED'] = ArrayList()
+    eventsDict['TASK_CREATED'].add('workflow.eventHandler.taskCreatedHandler.cl')
+    
+    for eventType in eventsDict:
         AppInfoSingleton.getAppInfo().\
         getActivitiEventScriptDictionary().\
-        put(getattr(ActivitiEventType,eventsList[i]), handlersList[i])
-
+        put(getattr(ActivitiEventType,eventType), eventsDict[eventType])
+       
 except:
     pass
 
