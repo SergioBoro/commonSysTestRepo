0a2b7d75abfd6f03b9189d0f292d101c32c50724
AleXXL <alexxl@curs.ru>
2015-06-02 12:50:05 +0000

    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/general@1066 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/lyra/basicForm.py
+++ score/lyra/basicForm.py
@@ -1,6 +1,17 @@
 # coding: utf-8
 
-class BasicForm():
+class BasicForm(object):
     u'''Базовый класс для любой формы, реализованной в Лире'''
-    def init(self):
+    def _getCursor(self, context):
+        '''TO BE OVERRIDEN'''
         pass
+
+    def _buildForm(self, context, request, response):
+        '''TO BE OVERRIDEN'''
+        pass
+
+    def setContext(self, session, main, add, elemetId):
+        self.session = session
+        self.main = main
+        self.add = add
+        self.elemetId = elemetId
--- score/lyra/cardForm.py
+++ score/lyra/cardForm.py
@@ -1,8 +1,56 @@
 # coding: utf-8
+from ru.curs.celesta.dbutils import XMLSerializer
+from java.lang import String
+from java.io import ByteArrayInputStream, ByteArrayOutputStream
 
 from lyra.basicForm import BasicForm
 
 class CardForm(BasicForm):
     u'''Базовый класс для формы типа карточка, реализованной в Лире'''
-    def init(self):
-        pass
+    def findRec(self, context):
+        c = self._getCursor(context)
+        c.navigate('-')
+        result = ByteArrayOutputStream()
+        XMLSerializer.serialize(c, result)
+        return result.toString("utf-8");
+
+    def move(self, context, cmd, data):
+        c = self._getCursor(context)
+
+        dataIS = ByteArrayInputStream(data.encode("UTF-8"))
+        XMLSerializer.deserialize(c, dataIS)
+
+        c2 = self._getCursor(context)
+        c2.copyFieldsFrom(c)
+        if c2.tryGetCurrent():
+            c2.copyFieldsFrom(c)
+            c2.update()
+        else:
+            c.insert()
+        c.navigate(cmd)
+        result = ByteArrayOutputStream()
+        XMLSerializer.serialize(c, result)
+        return result.toString("utf-8");
+
+    def newRec(self, context):
+        c = self._getCursor(context)
+        c.clear()
+        c.setRecversion(0)
+        result = ByteArrayOutputStream()
+        XMLSerializer.serialize(c, result)
+        return result.toString("utf-8");
+
+    def deleteRec(self, context, data):
+        c = self._getCursor(context)
+
+        dataIS = ByteArrayInputStream(data.encode("UTF-8"))
+        XMLSerializer.deserialize(c, dataIS)
+
+        c.delete()
+        if not c.navigate('>+'):
+            c.clear()
+            c.setRecversion(0)
+        result = ByteArrayOutputStream()
+        XMLSerializer.serialize(c, result)
+        return result.toString("utf-8");
+
