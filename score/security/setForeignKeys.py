b4e4432bb92cd1e30f268a9965cfa05c6987a43d
d.bozhenko <d.bozhenko@curs.ru>
2015-06-18 09:44:52 +0000

    убран лишний try finally
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1096 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/security/setForeignKeys.py
+++ score/security/setForeignKeys.py
@@ -11,11 +11,8 @@ def setForeignKeys(context):
     security_grain = score.getGrain('security')
     subject_table=security_grain.getTable("subjects")
     login_table=security_grain.getTable("logins")
-    try:
-        password_field = login_table.getColumn("password")
-        password_field.setNullableAndDefault(settings.isUseAuthServer())
-    except:
-        pass
+    password_field = login_table.getColumn("password")
+    password_field.setNullableAndDefault(settings.isUseAuthServer())
     employees_grain=score.getGrain(settings.getEmployeesParam("employeesGrain"))
     employees_table=employees_grain.getTable(settings.getEmployeesParam("employeesTable"))
     employees_id = employees_table.getColumn(settings.getEmployeesParam("employeesId"))
