34a68440191802d96c28ac331caddd37344932f9
a.rudenko <a.rudenko@curs.ru>
2016-03-14 13:24:16 +0000

    Убран лишний код из common.sys/score/common/__init__.py
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1418 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/__init__.py
+++ score/common/__init__.py
@@ -1,8 +1,4 @@
 from common.navigator import navigatorsParts, seriesNavigator
-from ru.curs.celesta import ConnectionPool
-from ru.curs.celesta import CallContext
-from ru.curs.celesta import SessionContext
-from ru.curs.celesta import Celesta
 from common.htmlhints.htmlHintsInit import permInit
 import initcontext
 
