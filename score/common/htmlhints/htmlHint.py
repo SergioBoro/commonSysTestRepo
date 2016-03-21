a6d2db0519000968bbcffb9552c752b7d2987e01
a.rudenko <a.rudenko@curs.ru>
2016-03-21 10:40:29 +0000

    Временное исправление ошибки связанной с кавычками в редакторе HTML(&laquo; &raquo;,)
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1430 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/htmlhints/htmlHint.py
+++ score/common/htmlhints/htmlHint.py
@@ -4,17 +4,11 @@ Created on 02.03.2016
 HTML подсказки
 @author: a.rudenko
 '''
-import hashlib
-from java.text import SimpleDateFormat
 import json
-import re
 
 from com.jayway.jsonpath import JsonPath
-from common.numbersseries.getNextNo import getNextNoOfSeries
 from common._common_orm import htmlHintsCursor
-from nci.functions.persons import checkSnilsUnique, getPersonIdBySid
 from ru.curs.celesta.showcase.utils import XMLJSONConverter
-from security._security_orm import subjectsCursor, loginsCursor
 from security.functions import userHasPermission
 
 
@@ -23,20 +17,22 @@ try:
 except:
     from ru.curs.celesta.showcase import JythonDTO
 
+
 def htmlHintElement(elementId):
     htmlHint = {
-            '@id': elementId,
-            '@type': 'xforms',
-            '@proc': 'common.htmlhints.htmlHint.cardData.celesta',
-            '@template':'common/htmlHints/htmlHint.xml',
-            '@hideOnLoad':'false',
-            'proc': {
-                '@id':'save',
-                '@name':'common.htmlhints.htmlHint.cardSave.celesta',
-                '@type':'SAVE'
-            }
+        '@id': elementId,
+        '@type': 'xforms',
+        '@proc': 'common.htmlhints.htmlHint.cardData.celesta',
+        '@template': 'common/htmlHints/htmlHint.xml',
+        '@hideOnLoad': 'false',
+        'proc': {
+            '@id': 'save',
+            '@name': 'common.htmlhints.htmlHint.cardSave.celesta',
+            '@type': 'SAVE'
         }
+    }
     return htmlHint
+
 
 def cardData(context, main=None, add=None, filterinfo=None, session=None, elementId=None):
     u'''Карточка HTML подсказки'''
@@ -44,47 +40,63 @@ def cardData(context, main=None, add=None, filterinfo=None, session=None, elemen
     htmlHints = htmlHintsCursor(context)
     if htmlHints.tryGet(elementId):
         htmlText = htmlHints.htmlText
-        htmlText = htmlText.replace('&lt;', '<').replace('&gt;', '>').replace('&lt', '<').replace('&gt', '>').replace('&nbsp;', ' ')
+        htmlText = htmlText\
+            .replace('&lt;', '<')\
+            .replace('&gt;', '>')\
+            .replace('&lt', '<')\
+            .replace('&gt', '>')\
+            .replace('&nbsp;', ' ')\
+            .replace('&laquo;', '"')\
+            .replace('&raquo;', '"')
         showOnLoad = htmlHints.showOnLoad
         if showOnLoad is None:
-            showOnLoad=0
+            showOnLoad = 0
     else:
         htmlText = u""
         showOnLoad = 0
     if userHasPermission(context, sid, 'htmlHintsEdit'):
-        userPerm=1
+        userPerm = 1
     else:
         userPerm = 0
-    xformsdata = {"schema":
-                      {"@xmlns":"",
-                        "htmlText":htmlText,
-                        "showHideHint":showOnLoad,
-                        "showOnLoad":showOnLoad,
-                        "showHideEdit":0,
-                        "userPerm":userPerm
-                        }
-                  }
+    xformsdata = {
+        "schema": {
+            "@xmlns": "",
+            "htmlText": htmlText,
+            "showHideHint": showOnLoad,
+            "showOnLoad": showOnLoad,
+            "showHideEdit": 0,
+            "userPerm": userPerm
+        }
+    }
 
-    xformssettings = {"properties":{"event":[{"@name":"single_click",
-                                              "@linkId": "1",
-                                              "action":{"@keep_user_settings": "true",
-                                                        "main_context": "current",
-                                                        "datapanel": {"@type": "current",
-                                                                      "@tab": "current"
-                                                                      }
-                                                        }
-                                              }]
-                                    }
-                      }
+    xformssettings = {
+        "properties": {
+            "event": [
+                {
+                    "@name": "single_click",
+                    "@linkId": "1",
+                    "action": {
+                        "@keep_user_settings": "true",
+                        "main_context": "current",
+                        "datapanel": {
+                            "@type": "current",
+                            "@tab": "current"
+                        }
+                    }
+                }
+            ]
+        }
+    }
 
     return JythonDTO(XMLJSONConverter.jsonToXml(json.dumps(xformsdata)),
                      XMLJSONConverter.jsonToXml(json.dumps(xformssettings)))
+
 
 def cardSave(context, main=None, add=None, filterinfo=None, session=None,
              elementId=None, xformsdata=None):
     u'''Сохранение HTML подсказки'''
     htmlHints = htmlHintsCursor(context)
-    
+
     htmlText = json.loads(xformsdata)["schema"]["htmlText"]
     showOnLoad = json.loads(xformsdata)["schema"]["showOnLoad"]
     if htmlHints.tryGet(elementId):
@@ -97,10 +109,16 @@ def cardSave(context, main=None, add=None, filterinfo=None, session=None,
         htmlHints.showOnLoad = showOnLoad
         htmlHints.insert()
 
+
 def htmlEdit(context, main, add, filterinfo, session, elementId):
     jsonData = json.loads(filterinfo)
     htmlText = jsonData['schema']['filter']['htmlText']
-    data = htmlText.replace('&lt;', '<').replace('&gt;', '>').replace('&lt', '<').replace('&gt', '>').replace('&nbsp;', ' ')#.replace('&nbsp', ' ')
+    data = htmlText\
+        .replace('&lt;', '<')\
+        .replace('&gt;', '>')\
+        .replace('&lt', '<')\
+        .replace('&gt', '>')\
+        .replace('&nbsp;', ' ')  # .replace('&nbsp', ' ')
     #raise Exception(left(data, 5))
     if data[:5] != '<div>':
         data = "<div>%s</div>" % data
@@ -108,6 +126,5 @@ def htmlEdit(context, main, add, filterinfo, session, elementId):
     <properties>
     </properties>
     '''
-    #raise Exception(main, add, filterinfo, session, elementId)
     res = JythonDTO(data, settings)
     return res
