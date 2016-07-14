59a5e61f7d53382dcd10898cbb1b26d5a76d3d74
Ivan <ivan@curs.ru>
2016-07-14 12:52:14 +0000

    отмена предыдущего коммита
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1515 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/lyra/cardForm.py
+++ score/lyra/cardForm.py
@@ -3,8 +3,7 @@ import ru.curs.lyra.BasicCardForm as BasicCardForm
 
 class CardForm(BasicCardForm):
     u'''Basic class for a card form'''
-    def __init__(self, context, session, main, add, elementId):
-        self.setContext(session, main, add, elementId)
+    def __init__(self, context):
         BasicCardForm.__init__(self, context)
     
     def setContext(self, session, main, add, elemetId):
--- score/lyra/gridForm.py
+++ score/lyra/gridForm.py
@@ -3,8 +3,7 @@ import ru.curs.lyra.BasicGridForm as BasicGridForm
 
 class GridForm(BasicGridForm):
     u'''Basic class for a grid form'''
-    def __init__(self, context, session, main, add, elementId):
-        self.setContext(session, main, add, elementId)
+    def __init__(self, context):
         BasicGridForm.__init__(self, context)
         
         
