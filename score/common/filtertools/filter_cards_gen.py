a25f0b8c8c8ab205b7b0a69c7cb70ba73ad2721d
s.gavrilov <s.gavrilov@curs.ru>
2016-09-09 09:59:05 +0000

    Исправлены ошибки переноса фильтра из ИАСБП в соmmоп.
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1556 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/filtertools/filter_cards_gen.py
+++ score/common/filtertools/filter_cards_gen.py
@@ -1,15 +1,20 @@
 # coding: utf-8
-from nsi.filtertools.filter import recovery
 import json
+
+from common.api.datapanels.grids import ToolbarItem
+from common.api.events.action import Action, ModalWindow
+from common.api.events.activities import DatapanelElement
+from common.filtertools.filter import recovery
+
 
 def card_info(context, elementId):
     u'''Функция переведения данных из context.getData в xforms'''
     filters = [
         x
         for x in context.getData()[elementId]
-            if x['@key'] == 'view'
+        if x['@key'] == 'view'
     ]
-    
+
     return filters
 
 
@@ -22,7 +27,7 @@ def card_save(xformsdata, context, filter_id):
         # но не будет использован фильтр. Для исключения избыточной проверки на наличие значений словаря вводим ключ-флаг.
         if not context.getData().get(u'card_save'):
             context.getData()[u'card_save'] = set([])
-        context.getData()[u'card_save'].add(filter_id)    
+        context.getData()[u'card_save'].add(filter_id)
         temp_context = xformsdata["schema"]["filters"]['filter']
         if isinstance(temp_context, dict):
             temp_context = [temp_context]
@@ -36,61 +41,44 @@ def card_save(xformsdata, context, filter_id):
                     stable_filter['@maxValue'] = temp_filter['@maxValue']
                     stable_filter['@boolInput'] = temp_filter['@boolInput']
                     stable_filter['item'] = temp_filter['item']
-                    stable_filter['@current_condition'] = temp_filter['@current_condition']
+                    stable_filter['@current_condition'] = temp_filter[
+                        '@current_condition']
                     # Проверка на наличие выделенных значений
                     if 'item' in temp_filter['items']:
-                        stable_filter['items']['item'] = temp_filter['items']['item']
+                        stable_filter['items'][
+                            'item'] = temp_filter['items']['item']
                     else:
                         stable_filter['items']['item'] = []
                     break
 
-def add_filter_buttons(filter_id, session, height=False, width=700, add_info=u'', add_context=None):   # в параметр heigth можно задавать число фильтров в *filter для гибкого отображения размеров окна 
+
+def add_filter_buttons(filter_id, session, height=False, width=700,
+                       add_info=u"", add_context=None, is_object=False):
     u'''
-    filter_id - id карточки в датапанели, 
-    height="300" - высота карточки фильтра
+        filter_id - id карточки в датапанели, 
+        height="300" - высота карточки фильтра
+        в параметр heigth можно задавать число фильтров в *filter для гибкого отображения размеров окна
     '''
     if isinstance(session, unicode) or isinstance(session, str):
         session = json.loads(session)['sessioncontext']
+    elif "sessioncontext" in session:
+        session = session["sessioncontext"]
     if not height:
-        height = int(0.6*float(session["sessioncontext"]['currentDatapanelHeight'] if "sessioncontext" in session else session['currentDatapanelHeight']))
+        height = int(0.6 * float(session['currentDatapanelHeight']))
     else:
         height *= 60
-    button = [
-        {
-            "@img": 'gridToolBar/formMagnify.png',
-            "@text": add_info or u"Параметры поиска",
-            "@hint": u"Установить фильтр",
-            "@disable": 'false',
-            "action":
-            {
-                "@show_in": "MODAL_WINDOW",
-                "#sorted":
-                [
-                    {"main_context":"current"},
-                    {
-                        "modalwindow"   :
-                        {   "@caption"  : u"Параметры поиска",
-                            "@height"   : height,
-                            "@width"    : width
-                        }
-                    },
-                    {
-                        "datapanel":
-                        {
-                            "@type": "current",
-                            "@tab": "current",
-                            "element": 
-                            {
-                                "@id": filter_id,
-                                "add_context": add_context or filter_id
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-    ]
-    
+
+    button = ToolbarItem("deleteFileButton")\
+        .setCaption(add_info or u"Параметры поиска")\
+        .setHint(u"Установить фильтр")\
+        .setAction(Action()
+            .add(DatapanelElement(filter_id,
+                                add_context or filter_id))
+            .showIn(ModalWindow(u"Параметры поиска",
+                                width, height)))\
+        .setImage("gridToolBar/filter.png")
+
+    if not is_object:
+        button = button.toJSONDict()
+
     return button
-
-
