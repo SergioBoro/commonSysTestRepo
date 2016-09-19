9d120350064c47fbbd9bc7e66c831ec54297a762
m.prudyvus <m.prudyvus@curs.ru>
2016-09-19 12:51:40 +0000

    функция, определяющая тип данных в курсоре (поддерживаются вьюхи и таблицы)
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1584 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/sysfunctions.py
+++ score/common/sysfunctions.py
@@ -1,8 +1,11 @@
 # coding: utf-8
-import os
-import json
-from ru.curs.celesta import Celesta
 import importlib
+import json
+import os
+
+from ru.curs.celesta import Celesta
+from ru.curs.celesta.dbutils import Cursor, ViewCursor
+
 
 def toHexForXml(s):
     '''Функция модифицирует спецсимволы в строке в формат пригодный для имен тегов xml'''
@@ -10,14 +13,15 @@ def toHexForXml(s):
     for ch in s:
         numCh = ord(ch)
         if numCh not in xrange (48, 58) and\
-        numCh not in xrange (65, 91) and\
-        numCh not in xrange (97, 123) and \
-        numCh not in xrange (1040, 1104):
+                numCh not in xrange (65, 91) and\
+                numCh not in xrange (97, 123) and \
+                numCh not in xrange(1040, 1104):
             lst.append('_x%s_' % ('000%s' % hex(numCh)[2:])[-4:])
         else:
             lst.append(ch)
 
-    return reduce(lambda x, y:x + y, lst)
+    return reduce(lambda x, y: x + y, lst)
+
 
 def tableCursorImport(grainName, tableName):
     u'''Функция, импортирующая  класс курсора на таблицу'''
@@ -31,12 +35,14 @@ def tableCursorImport(grainName, tableName):
 
     return getattr(_grain_orm, "%sCursor" % tableName)
 
+
 def objectImport(path):
     u'''Импорт объекта по пути пакет.модуль.....функция.'''
-    
-    (module_name, fun_name) = (".".join(path.split(".")[:-1]),path.split(".")[-1])
-    
+
+    (module_name, fun_name) = (".".join(path.split(".")[:-1]), path.split(".")[-1])
+
     return getattr(importlib.import_module(module_name), fun_name)
+
 
 def getGridWidth(session, delta=51):
     u"""Функция получает ширину грида, в зависимости от ширины датапанели."""
@@ -45,15 +51,17 @@ def getGridWidth(session, delta=51):
     else:
         return unicode(int(session["sessioncontext"]["currentDatapanelWidth"]) - delta) + "px"
 
+
 def getGridHeight(session, numberOfGrids=1, gridHeaderHeight=55, delta=59):
     u"""Функция получает высоту грида, в зависимости от высоты датапанели."""
     # raise Exception(session)
     if not isinstance(session, dict):
-        return unicode(int((int(json.loads(session)["sessioncontext"]["currentDatapanelHeight"])\
-                         - gridHeaderHeight) / numberOfGrids) - delta)
+        return unicode(int((int(json.loads(session)["sessioncontext"]["currentDatapanelHeight"])
+                            - gridHeaderHeight) / numberOfGrids) - delta)
     else:
-        return unicode(int((int(session["sessioncontext"]["currentDatapanelHeight"])\
-                         - gridHeaderHeight) / numberOfGrids) - delta)
+        return unicode(int((int(session["sessioncontext"]["currentDatapanelHeight"])
+                            - gridHeaderHeight) / numberOfGrids) - delta)
+
 
 def getSettingsPath():
     u"""Функция возвращает путь к файлу с настройками гранул."""
@@ -64,3 +72,15 @@ def getSettingsPath():
     except:
         settingsPath = Celesta.getInstance().setupProperties.getProperty('grainssettings.path')
     return settingsPath
+
+
+def getFieldType(cursor, field):
+    u"""Функция для получения типа поля курсора
+        (хранится по разному в таблице и вьюхе)"""
+    meta = cursor.meta()
+    if isinstance(cursor, Cursor):
+        field_type = meta.getColumn(field).celestaType
+    elif isinstance(cursor, ViewCursor):
+        field_type = meta.columns[field].celestaType
+
+    return unicode(field_type)
