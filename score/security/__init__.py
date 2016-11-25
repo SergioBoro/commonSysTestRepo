01eefdaa8681e85697f0f9933dbaa1754d3f8dc9
r.tugushev <r.tugushev@curs.ru>
2016-11-25 02:45:46 +0000

    Добавлена страница логина. Подправлен скрипт для заливки тестовых данных о событиях.
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1738 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/security/__init__.py
+++ score/security/__init__.py
@@ -12,7 +12,7 @@ from ru.curs.celesta import CallContext
 from ru.curs.celesta import SessionContext
 from ru.curs.celesta import Celesta
 import initcontext
-
+  
 settings = Settings()
 if not settings.loginIsSubject() and settings.isEmployees():
     #Если роли привязываются к сотруднику и таблица сотрудников настроена (нужные настройки есть в grainSettings.xml),
@@ -20,15 +20,15 @@ if not settings.loginIsSubject() and settings.isEmployees():
     employeesGrain = settings.getEmployeesParam("employeesGrain")
     employeesTable = settings.getEmployeesParam("employeesTable")
     employeesCursor = tableCursorImport(employeesGrain, employeesTable)
-
+  
     employeesCursor.onPostInsert.append(employeesSubjectsPostInsert)
     employeesCursor.onPostUpdate.append(employeesSubjectsPostUpdate)
     employeesCursor.onPreDelete.append(employeesSubjectsPreDelete)
-
+  
 if not settings.isSystemInitialised():
     context = initcontext()
     setConstraint(context) #функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
     securityInit(context) # добавление в базу необходимых для работы гранулы данных
-
+  
 navigatorsParts['securityNavigator'] = authentificationNavigator
 
