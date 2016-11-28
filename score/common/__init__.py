707242732acf931d735e969ba16357f75a6a75c0
m.prudyvus <m.prudyvus@curs.ru>
2016-11-28 16:30:17 +0000

    изменено имя группы пункта навигатора на нормальное
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1739 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/__init__.py
+++ score/common/__init__.py
@@ -1,7 +1,7 @@
 from common.navigator import navigatorsParts, seriesNavigator
 from common.htmlhints.htmlHintsInit import permInit
 import initcontext
-   
+
 context = initcontext()
 permInit(context)
-navigatorsParts['96'] = seriesNavigator
+navigatorsParts['numberSeries'] = seriesNavigator
