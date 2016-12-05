bb9881ed9b3212b755278a3509a83c144a7c65b3
m.prudyvus <m.prudyvus@curs.ru>
2016-12-05 09:25:21 +0000

    Добавлен декоратор для обработки групп навигатора
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1764 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/sysfunctions.py
+++ score/common/sysfunctions.py
@@ -1,10 +1,8 @@
 # coding: utf-8
 import importlib
 import json
-import os
 
 from ru.curs.celesta import Celesta
-from ru.curs.celesta.dbutils import Cursor, ViewCursor
 
 
 def toHexForXml(s):
@@ -12,9 +10,9 @@ def toHexForXml(s):
     lst = []
     for ch in s:
         numCh = ord(ch)
-        if numCh not in xrange (48, 58) and\
-                numCh not in xrange (65, 91) and\
-                numCh not in xrange (97, 123) and \
+        if numCh not in xrange(48, 58) and\
+                numCh not in xrange(65, 91) and\
+                numCh not in xrange(97, 123) and \
                 numCh not in xrange(1040, 1104):
             lst.append('_x%s_' % ('000%s' % hex(numCh)[2:])[-4:])
         else:
@@ -72,3 +70,25 @@ def getSettingsPath():
     except:
         settingsPath = Celesta.getInstance().setupProperties.getProperty('grainssettings.path')
     return settingsPath
+
+
+def defaultGroup(navigator_generate_function):
+    """
+    Обрабатывает данные из навигатора
+    превращает пустой root навигатора в отсутствующую группу
+    :param navigator_generate_function: группа навигатора
+    :return: возвращает группу навигатора, обработанную toJSONDict
+    """
+    _empty_group = {"group": None}
+
+    def wrapped(*args, **kwargs):
+        root_node = navigator_generate_function(*args, **kwargs)
+        # rootNode - это root. Он есть всегда.
+        # Но в нём может не быть самой группы
+        if not root_node.getNumberOfChildren():
+            return _empty_group
+        # или группа может быть пустой
+        if not root_node.getChildren()[0].getNumberOfChildren():
+            return _empty_group
+        return root_node.toJSONDict()
+    return wrapped
