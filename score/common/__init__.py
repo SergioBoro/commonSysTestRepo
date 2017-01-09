166fce9d937bb865878fab581604c1eef85031eb
s.gavrilov <s.gavrilov@curs.ru>
2017-01-09 14:56:16 +0000

    Добавлена функция для изменения и логирования кожффициентов.
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@1819 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/common/__init__.py
+++ score/common/__init__.py
@@ -1,9 +1,9 @@
-from common.navigator import navigatorsParts, seriesNavigator
-from common.htmlhints.htmlHintsInit import permInit
-import initcontext
-
-context = initcontext()
-
-if not isinstance(context, (str, unicode)):
-    permInit(context)
-navigatorsParts['numberSeries'] = seriesNavigator
+# from common.navigator import navigatorsParts, seriesNavigator
+# from common.htmlhints.htmlHintsInit import permInit
+# import initcontext
+# 
+# context = initcontext()
+# 
+# if not isinstance(context, (str, unicode)):
+#     permInit(context)
+# navigatorsParts['numberSeries'] = seriesNavigator
--- score/security/__init__.py
+++ score/security/__init__.py
@@ -1,34 +1,34 @@
 # coding: utf-8
-from common.navigator import navigatorsParts
-from ru.curs.celesta.syscursors import RolesCursor
-from security.functions import Settings
-from security.navigator import authentificationNavigator
-from security.setForeignKeys import setForeignKeys as setConstraint
-from security.xform.users import employeesSubjectsPostInsert, employeesSubjectsPostUpdate, employeesSubjectsPreDelete
-from common.sysfunctions import tableCursorImport
-from security.securityInit import securityInit
-from ru.curs.celesta import ConnectionPool
-from ru.curs.celesta import CallContext
-from ru.curs.celesta import SessionContext
-from ru.curs.celesta import Celesta
-import initcontext
-  
-settings = Settings()
-if not settings.loginIsSubject() and settings.isEmployees():
-    #Если роли привязываются к сотруднику и таблица сотрудников настроена (нужные настройки есть в grainSettings.xml),
-    #привязываем к ней триггеры. 
-    employeesGrain = settings.getEmployeesParam("employeesGrain")
-    employeesTable = settings.getEmployeesParam("employeesTable")
-    employeesCursor = tableCursorImport(employeesGrain, employeesTable)
-  
-    employeesCursor.onPostInsert.append(employeesSubjectsPostInsert)
-    employeesCursor.onPostUpdate.append(employeesSubjectsPostUpdate)
-    employeesCursor.onPreDelete.append(employeesSubjectsPreDelete)
-  
-if not settings.isSystemInitialised():
-    context = initcontext()
-    setConstraint(context) #функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
-    securityInit(context) # добавление в базу необходимых для работы гранулы данных
-  
-navigatorsParts['securityNavigator'] = authentificationNavigator
+# from common.navigator import navigatorsParts
+# from ru.curs.celesta.syscursors import RolesCursor
+# from security.functions import Settings
+# from security.navigator import authentificationNavigator
+# from security.setForeignKeys import setForeignKeys as setConstraint
+# from security.xform.users import employeesSubjectsPostInsert, employeesSubjectsPostUpdate, employeesSubjectsPreDelete
+# from common.sysfunctions import tableCursorImport
+# from security.securityInit import securityInit
+# from ru.curs.celesta import ConnectionPool
+# from ru.curs.celesta import CallContext
+# from ru.curs.celesta import SessionContext
+# from ru.curs.celesta import Celesta
+# import initcontext
+#   
+# settings = Settings()
+# if not settings.loginIsSubject() and settings.isEmployees():
+#     #Если роли привязываются к сотруднику и таблица сотрудников настроена (нужные настройки есть в grainSettings.xml),
+#     #привязываем к ней триггеры. 
+#     employeesGrain = settings.getEmployeesParam("employeesGrain")
+#     employeesTable = settings.getEmployeesParam("employeesTable")
+#     employeesCursor = tableCursorImport(employeesGrain, employeesTable)
+#   
+#     employeesCursor.onPostInsert.append(employeesSubjectsPostInsert)
+#     employeesCursor.onPostUpdate.append(employeesSubjectsPostUpdate)
+#     employeesCursor.onPreDelete.append(employeesSubjectsPreDelete)
+#   
+# if not settings.isSystemInitialised():
+#     context = initcontext()
+#     setConstraint(context) #функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
+#     securityInit(context) # добавление в базу необходимых для работы гранулы данных
+#   
+# navigatorsParts['securityNavigator'] = authentificationNavigator
 
