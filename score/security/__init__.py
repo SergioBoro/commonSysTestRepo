e82e35ce519a13d0e79d99a64154c223e623a4a6
m.prudyvus <m.prudyvus@curs.ru>
2017-05-04 10:52:57 +0000

    undo last commit remove unused imports
    git-svn-id: https://share.curs.ru/svn/grains/celestabasic/trunk/common.sys@2015 bf7a05a2-0d8f-8241-91a8-991a96a8b1b0

--- score/security/__init__.py
+++ score/security/__init__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from common.navigator import navigatorsParts
+from common.sysfunctions import tableCursorImport
 from security.functions import Settings
 from security.navigator import authentificationNavigator
 from security.securityInit import securityInit
@@ -12,23 +13,23 @@ try:
 except ImportError:
     isInitContext = False
 
-# settings = Settings()
-# if not settings.loginIsSubject() and settings.isEmployees():
-#     #Если роли привязываются к сотруднику и таблица сотрудников настроена (нужные настройки есть в grainSettings.xml),
-#     #привязываем к ней триггеры.
-#     employeesGrain = settings.getEmployeesParam("employeesGrain")
-#     employeesTable = settings.getEmployeesParam("employeesTable")
-#     employeesCursor = tableCursorImport(employeesGrain, employeesTable)
-#
-#     employeesCursor.onPostInsert.append(employeesSubjectsPostInsert)
-#     employeesCursor.onPostUpdate.append(employeesSubjectsPostUpdate)
-#     employeesCursor.onPreDelete.append(employeesSubjectsPreDelete)
-#
-# if not settings.isSystemInitialised():
-#     if isInitContext:
-#         context = initcontext()
-#         setConstraint(context) #функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
-#         securityInit(context) # добавление в базу необходимых для работы гранулы данных
-     
-navigatorsParts['securityNavigator'] = authentificationNavigator
+settings = Settings()
+if not settings.loginIsSubject() and settings.isEmployees():
+    # Если роли привязываются к сотруднику и таблица сотрудников настроена (нужные настройки есть в grainSettings.xml),
+    # привязываем к ней триггеры.
+    employeesGrain = settings.getEmployeesParam("employeesGrain")
+    employeesTable = settings.getEmployeesParam("employeesTable")
+    employeesCursor = tableCursorImport(employeesGrain, employeesTable)
 
+    employeesCursor.onPostInsert.append(employeesSubjectsPostInsert)
+    employeesCursor.onPostUpdate.append(employeesSubjectsPostUpdate)
+    employeesCursor.onPreDelete.append(employeesSubjectsPreDelete)
+
+if not settings.isSystemInitialised():
+    if isInitContext:
+        context = initcontext()
+        setConstraint(
+            context)  # функция устанавливает внешний ключ в таблицу subjects и меняет значение параметра isSystemInitialised на True
+        securityInit(context)  # добавление в базу необходимых для работы гранулы данных
+
+navigatorsParts['securityNavigator'] = authentificationNavigator
